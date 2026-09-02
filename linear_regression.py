b1fffd878f6b5f11555fb0d1e77a71e8c50b8c34
vesc0 <dveselinovic84@gmail.com>
2026-09-02 02:24:28 +0200

    update linear reg and naive bayes

--- linear_regression.py
+++ linear_regression.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 
 #%% UCENJE
-def learn(X, y):
+def learn(X, y, lam=0, stochastic=False, auto_alpha=True, alpha=None):
     model = {}
-    
+    X = X.copy()
     X_mean = X.mean()
     X_std = X.std()
     X = (X - X_mean) / X_std
@@ -15,21 +15,54 @@ def learn(X, y):
     X = X.to_numpy()     
     y = y.to_numpy()
     
+    reg_mask = np.ones((1, n))
+    reg_mask[0, -1] = 0
+    
     # INIT
     w = np.random.random((1,n))
-    alpha = 0.6
+    if alpha is None:
+        alpha = 0.05 if stochastic else 0.06
+    prev_cost = np.inf
+    w_prev = w.copy()
+    alpha0 = alpha
     
     # ALGORITAM ZA UCENJE: Gradient Descent
     for it in range(10000):
-    	pred = X.dot(w.T)
-    	err = pred - y
-    	grad = err.T.dot(X) / m
-    	w = w - alpha*grad
-    	
-    	MSE = err.T.dot(err) / m
-    	grad_norm = abs(grad).sum()
-    	print(it, grad_norm, MSE)
-    	if grad_norm < 0.01: break
+        if stochastic:
+            j = np.random.randint(m)
+            Xb = X[j:j+1]      # oblik (1, n)
+            yb = y[j:j+1]      # oblik (1, 1)
+        else:
+            Xb = X
+            yb = y
+        pred = Xb.dot(w.T)
+        err = pred - yb
+        grad = err.T.dot(Xb) / len(Xb) #m
+        grad = grad + 2 * lam * w * reg_mask
+        
+        MSE = err.T.dot(err) / len(Xb) #m
+        penalty = lam * ((w * reg_mask) ** 2).sum()
+        cost = MSE + penalty
+        
+        if auto_alpha and not stochastic:
+            c = cost.item()
+            if c > prev_cost:
+                w = w_prev # ponisti prosli korak
+                prev_cost = np.inf # sledeci korak neka prodje
+                alpha *= 0.5
+                continue
+            if auto_alpha and stochastic:
+                alpha = alpha0 / (1 + it * 0.001)
+            else:
+                alpha *= 1.05
+                prev_cost = c
+                w_prev = w.copy()
+        
+        w = w - alpha*grad
+        
+        grad_norm = abs(grad).sum()
+        print(it, grad_norm, MSE, cost)
+        if not stochastic and grad_norm < 0.01: break
     
     model['w'] = w
     model['mean'] = X_mean
@@ -39,17 +72,30 @@ def learn(X, y):
 
 #%% PREDVIDJANJE
 def predict(model, X):
+    X = X.copy()
     X = (X - model['mean']) / model['std']
     X['X0'] = 1
     
     return X.to_numpy().dot(model['w'].T)
     
 #%% KORISCENJE
-data = pd.read_csv('data/house.csv')
-X = data.drop('Price', axis=1)
-y = data[['Price']]
-model = learn(X, y)
+data = pd.read_csv('data/boston.csv')
 
-data_new = pd.read_csv('data/house_new.csv')
-data_new['price'] = predict(model, data_new)
-print(data_new)
+# Podela skupa na trening i test
+data = data.sample(frac=1, random_state=42)
+split = int(0.8 * len(data))
+train = data[:split]
+test  = data[split:]
+
+X_train = train.iloc[:, :-1]
+y_train = train.iloc[:, -1:]
+X_test  = test.iloc[:, :-1]
+y_test  = test.iloc[:, -1:]
+
+# Ucenje na trening skupu
+model = learn(X_train, y_train, lam=0.0001)
+
+# Predikcija na testu
+pred = predict(model, X_test)
+print(pred[:10])
+print(y_test.to_numpy()[:10])
--- naive_bayes.py
+++ naive_bayes.py
@@ -1,17 +1,26 @@
 import pandas as pd
+import numpy as np
+from scipy.stats import norm
 
 #%% UCENJE
-def learn(data, class_attr):
+def learn(data, class_attr, smoothing=1):
     model = {}
+    model['_types'] = {}
     
-    apriori = data[class_attr].value_counts()
+    apriori = data[class_attr].value_counts() + smoothing
     apriori /= apriori.sum()
     model['_apriori'] = apriori
     
     for attribute in data.drop(class_attr, axis=1).columns:
-        mat_cont = pd.crosstab(data[attribute], data[class_attr])
-        mat_cont /= mat_cont.sum()
-        model[attribute] = mat_cont
+        if pd.api.types.is_numeric_dtype(data[attribute]):
+            mean_var = data.groupby(class_attr)[attribute].agg(['mean', 'var'])
+            model[attribute] = mean_var
+            model['_types'][attribute] = 'numeric'
+        else:
+            mat_cont = pd.crosstab(data[attribute], data[class_attr]) + smoothing
+            mat_cont /= mat_cont.sum()
+            model[attribute] = mat_cont
+            model['_types'][attribute] = 'categorical'
     
     return model
 
@@ -19,30 +28,50 @@ def learn(data, class_attr):
 def predict(model, new_instance):
     class_probabilities = {}
     
+    epsilon = 1e-9
     for class_value in model['_apriori'].index:
-        probability = 1
+        log_probability = 0
         
         for attribute in model:
+            if attribute == '_types':
+                continue
             if attribute == '_apriori':
-                probability *= model['_apriori'][class_value]
+                p = model['_apriori'][class_value]
             else:
-                probability *= model[attribute][class_value][new_instance[attribute]]
-        class_probabilities[class_value] = probability
+                if model['_types'][attribute] == 'numeric':
+                    mean = model[attribute].loc[class_value, 'mean']
+                    var = model[attribute].loc[class_value, 'var']
+                    var = max(var, epsilon)
+                    p = norm.pdf(new_instance[attribute], loc=mean, scale=np.sqrt(var))
+                else:
+                    # kategoricki
+                    p = model[attribute][class_value][new_instance[attribute]]
+                
+            log_probability += np.log(p) if p > 0 else np.log(epsilon)
+            
+        class_probabilities[class_value] = log_probability
     
     prediction = max(class_probabilities, key=class_probabilities.get)
     
     return prediction, class_probabilities
 
 #%% KORISCENJE
-data = pd.read_csv('data/prehlada.csv')
-model = learn(data, 'Prehlada')
+data = pd.read_csv('data/drug.csv')
+class_attr = data.columns[-1]
 
-data_new = pd.read_csv('data/prehlada_novi.csv')
+# Podela skupa na trening i test
+data = data.sample(frac=1, random_state=42).reset_index(drop=True)
+split = int(len(data) * 0.8)
+train, test = data[:split], data[split:].reset_index(drop=True)
+
+# Ucenje na trening skupu
+model = learn(train, class_attr, smoothing=0)
+
+# Predikcija na testu
+data_new = test
 for i in range(len(data_new)):
     prediciton, confidence = predict(model, data_new.iloc[i])
     
     data_new.loc[i, 'prediction'] = prediciton
     for klasa in confidence:
         data_new.loc[i, 'class='+klasa] = confidence[klasa]
-
-print(data_new)
